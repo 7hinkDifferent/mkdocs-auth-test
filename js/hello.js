98653cef910d8c3e8fed4c919cc429ea863a465b
7hinkDifferent <627238459@qq.com>
2024-12-23 21:49:41 +0800

    updated

--- js/hello.js
+++ js/hello.js
@@ -28,7 +28,7 @@ function joinUrl (base, path) {
 
 // 加载 search_index.json
 // fetch("/search/search_index.json")
-fetch(joinUrl(base_url, "search/search_index.json"))
+fetch(joinUrl(base_url, "search_index.json"))
   .then((response) => response.json())
   .then((searchIndex) => {
     console.log("Original search index:", searchIndex);
